c21997cd12d286d5896a183744705929d9fa831b
thijsrijkers <gamearea20@gmail.com>
2020-06-04 10:41:21 +0200

    Stage assets edit

--- src/DeathSmashBros/Engine/Players/BotPlayer.cs
+++ src/DeathSmashBros/Engine/Players/BotPlayer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DeathSmashBros.Engine.Players
@@ -10,7 +11,11 @@ namespace DeathSmashBros.Engine.Players
     {
         public override void Update()
         {
-            throw new NotImplementedException(); // TODO
+            while (true)
+            {
+
+                Thread.Sleep(1000);
+            }
         }
     }
 }
